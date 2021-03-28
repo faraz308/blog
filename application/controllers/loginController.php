bd49ac8826daee7faa1ed9a4ff983367d7509dfa
faraz khan <farazkhan.et@gmail.com>
2021-03-28 23:54:37 +0530

    create login and dashboard page
    created login controller and done validation with form and database, also created logout and dashborad screen

--- application/controllers/loginController.php
+++ application/controllers/loginController.php
@@ -8,10 +8,40 @@ class loginController extends CI_Controller {
 		
 	}
    
-    public function index() {
+	public function authenticate(){
+	
+		$this->form_validation->set_rules('userName', 'Username', 'trim|required|min_length[5]|max_length[30]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+		$this->form_validation->set_error_delimiters('<div class="error">', '</div>');
+		$this->form_validation->set_message('required', 'Enter %s');
+		if ($this->form_validation->run() === FALSE)
+		{  
+		    $this->load->view('login');
+		}
+		else
+		{   
+			$data = [
+			'userName' => $this->input->post('userName'),
+			'password' => md5($this->input->post('password')),
+			];
+			
+			$this->load->model('login');
+			$check = $this->login->auth_check($data);
+			//var_dump($check);exit;
+			if($check != false ){
+				$sessionData = array(
+				'user_id' => $check->id,
+				'user_name' => $check->userName
+				);
+				$this->session->set_userdata($sessionData);
+				redirect( base_url('/dashboardController') ); 
+			} else {
+				$this->session->set_flashdata('wrongcreds','userName and Password doesn\'t match');
+			}
+			$this->load->view('login');
+		}
 		
-        $this->load->view('login');
-    }
+	}
 
 
 }	
--- application/views/login.php
+++ application/views/login.php
@@ -5,22 +5,29 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+		<style>
+		.error {
+			color:red;
+		}
+		</style>
     </head>
     <body>
         <div class="container">
             <div class="column">
-                <p></p>
-                <form method="post" action="<?= site_url('authenticate')?>">
+                <p class="error"> <?php echo $this->session->flashdata('wrongcreds');?> </p>
+                <form method="post" action="<?php site_url('loginController/authenticate')?>">
                     <div class="field">
                         <label class="label">Username</label>
                         <div class="control">
-                            <input class="input" type="text" placeholder="Text input">
+                            <input class="input" type="text" value="<?php echo $this->input->post('userName') ?>" name="userName" maxlength="30" placeholder="Enter Username">
+							 <?php echo form_error('userName'); ?> 
                         </div>
                     </div>
                     <div class="field">
                         <label class="label">Password</label>
                         <div class="control">
-                            <input class="input" type="passport" placeholder="Text input">
+                            <input class="input" type="password" name="password" placeholder="Enter password">
+							<?php echo form_error('password'); ?>
                         </div>
                     </div>
                     <div class="field is-grouped">
